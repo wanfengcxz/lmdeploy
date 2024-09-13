6f2647b7f95bf9a90cdd94078c9bf962e1650226
wanfengcxz <2917021186@qq.com>
2024-09-13 15:12:28 +0800

    update

--- run_internlm2.py
+++ run_internlm2.py
@@ -6,8 +6,8 @@ if __name__ == "__main__":
     # torch.set_printoptions(precision=10)
     pipe = lmdeploy.pipeline("/root/.cache/modelscope/hub/Shanghai_AI_Laboratory/internlm2-chat-7b",
                              backend_config = PytorchEngineConfig(tp=1,block_size=16, cache_max_entry_count=0.3, device_type="camb"))
-    question = ["Hi, pls intro yourself in detail", "Please introduce Shanghai."]
-    #question = ["Hi, pls intro yourself in detail"]
+    #question = ["Hi, pls intro yourself in detail", "Please introduce Shanghai."]
+    question = ["Hi, pls intro yourself in detail"]
     print(question)
     response = pipe(question, do_preprocess=False, top_k=1)
     print(response)
